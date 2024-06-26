11c579726c84a7916df8cafe4f4f46a0a98ad740
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-06-26 05:49:31 +0000

    datatype conversion

--- 01_basics/03_conversionDatatype.js
+++ 01_basics/03_conversionDatatype.js
@@ -1,30 +1,59 @@
-let abc = undefined
+// let abc = undefined
 
-//detect its datatype as youre working at backend 
+// //detect its datatype as youre working at backend 
 
-// const {score1}= req.body //no idea what datatype sccore is 
+// // const {score1}= req.body //no idea what datatype sccore is 
 
 
-//detecting datatype of score
-// console.log(typeof (score))
+// //detecting datatype of score
+// // console.log(typeof (score))
 
-let valueInNumber = Number(abc)
-// console.log(typeof abc)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let valueInNumber = Number(abc)
+// // console.log(typeof abc)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-// "33" => 33
-// "33abc" => NaN
-// true =>1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true =>1; false => 0
 
-let isLoggedIn= 1 
-let booleanIsLoggedIn= Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+// let isLoggedIn= 1 
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
 
-//if value of the variable is kept empty then the output comes out as zero
+// //if value of the variable is kept empty then the output comes out as zero
 
-let someNumber = 44
+// let someNumber = 44
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//********** Operations ***************
+let value = 3 
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " sandy"
+
+let str3 = str1 + str2
+console.log(str3)
+console.log( 1 + "2") //problem nahi ayegi
+console.log("1" + "2") //problem nahi ayegi
+console.log("1" + "2") //problem nahi ayegi
+console.log("1" + 2 + 2)//string mai convert ho jayengi saari
+
+//but 
+console.log(1 + 2 + "2")// string se pehle wali add ho jayengi then string se concatinate hongi
+
+console.log(1 + 2 + "2"+ 1 + 2) // 1+2 = 3 
+//output 3212
+
+// ********** bad codes ***********
+
+// console.log(+true)//tricky conversion nahi karna chahiye
+// console.log(+"")//tricky conversion nahi karna chahiye 
+
+ 
