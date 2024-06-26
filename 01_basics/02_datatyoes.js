dc9527d7f2c2c5aae6d31c9acfc42e0589db0da5
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-06-26 03:23:11 +0000

    datatypes

---
+++ 01_basics/02_datatyoes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //wea reusing node js and not browser 
+
+//avoid semicolons //code readability should be high 
+
+let name ="sandeep"
+let age =18
+let isLoggedIN=false
+
+//number => 2 to power 53
+//big int 
+//string => ""
+//bollean => true/false
+//null - this is a value as well as a datatype 
+//undefined - value of a not defined datatype 
+let state //undefined 
+let state2 = null //null
+//symbol //used in react mostly for uniqueness 
+//primitive datatypes ^
+
+//object 
+
+console.log(typeof null); //language ki khaami hai ye ko ye object hai
