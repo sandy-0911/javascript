cec53d121876e67a61af983e5d8d26cbad3c44a5
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-06-29 05:35:27 +0000

    Comparion of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(null > 0);// 0 > 0 false ayega 
+console.log(null == 0);
+console.log(null >= 0);// 0 >= 0 true ayega
+
+//unedefined ke aage > < laga do wo koi value mai convert nahi hota 
+//wo zero hi dega 
+
+// strict check
+//also checks data type
+console.log("2"==2)//true 
+console.log("2"===2)//false 
