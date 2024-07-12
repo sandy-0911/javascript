f4a561256cb5b05887f72126f6791afbb429ed05
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-07-12 03:36:00 +0000

    modified

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -2,12 +2,13 @@
     //console.log(`DB connected`)
 })(); //iffi ban gya ab ye : immediately Invoked Function Expression IIFE
 //semi colon lagana is zaruri
+//to stop the infinte loop of called function 
 
 ( () => {
     //console.log(`DB Connected TWO`)
-}  )()
+}  )()//arguements pass hote hai isme
 
-
+//arrow functions are also called expressions
 ( (name) => {
     //unamed IFFI
     console.log(`DB Connected TWO  ${name}`)
