74916c3b931b1a9da99d47a9c81a303aa2e8d45e
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-07-13 09:12:04 +0000

    Control Flow

---
+++ 03_basics/05_controlFlow.js
@@ -0,0 +1,52 @@
+//if
+// const isUserLoggedIn = true
+// if (3 != 2){
+
+// }
+//all relatioinal operators <, >, <=, >=, ==, !=
+//=== type bhi check karega 
+
+
+//new shit
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),
+// console.log("test2");
+
+//nesting
+
+
+
+// const isUserLoggedIn = true
+// const possesingDebitCard = true 
+
+// if (isUserLoggedIn && possesingDebitCard){
+//     console.log("Allow to buy course")
+// }
+
+//falsy values 
+// false ,0 ,-0, BigInt 0n, "", null, undefined, NaN
+
+//truthy  values - string ke andar koi bhi value truthy value hoti hai 
+//"0", "false", " ", {} , [ ] , function (){}
+
+// if you want to check whether an object is empty or not 
+// const emptyObject = {}
+
+// if (Object.keys(emptyObject).length === 0){
+//     console.log(`Object is Empty`)
+// }
+
+
+//NUllish Coalescing Operator(??): null defined
+
+// let val1;
+// // val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// //safety check krta hai null value ki
+// //sirf null and undefined ke liye bani hai
+// console.log(val1)
+
+//ternary operator
+
