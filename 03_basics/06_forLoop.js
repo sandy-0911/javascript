8eaab423f7f887b0549ebd9c613ebb22fb88cdf0
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2024-07-13 16:00:59 +0000

    changes done

---
+++ 03_basics/06_forLoop.js
@@ -0,0 +1,9 @@
+// for 
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}// normal for loop of every language 
+
+//elements inside loop are not accessible outside 
+//break and continue
